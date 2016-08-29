f21ba18843cbec5ab5d95477defd00637cf3ed24
Felix Sittenauer <ga78vez@mytum.de>
2016-08-29 14:42:38 +0800

    GPIOPin

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <string>
 #include "../IGpio/IGpio.h"
+#include "../GpioPin/GpioPin.h"
 
-int main()
-{
-	IGpio gpioInterface;
-}
+int main() {
+	IGpio gpioInterface();
+	GpioPin pin(17, 1, gpioInterface);
+};
