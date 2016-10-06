4a113e95f5bf7b8b80b6322f5d6a593583f64ec1
Felix Sittenauer <ga78vez@mytum.de>
2016-10-06 21:56:11 +0200

    update

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include "../GPIO/IGpio/IGpio.h"
 #include "../FlightController/FlightController.h"
+#include "../sensor/MPU6050/MPU6050.h"
 
 
 int main () {
@@ -10,10 +11,10 @@ int main () {
 
 	FlightController flightCtrl (IGpioInstance);
 	//flightCtrl.programMotors();
-	/*MPU6050 mpu6050 (IGpioInstance);
+	MPU6050 mpu6050 (2, IGpioInstance);
 	mpu6050.wakeUp();
-	mpu6050.calibrate();
-	mpu6050.startLoop();*/
+	mpu6050.calibrateAccelerometer()
+	mpu6050.startLoop();
 
 	return 0;
 }
