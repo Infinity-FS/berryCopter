36c81a8cfb50f6b053798bd1a6e2aa59b8b8fc60
Felix Sittenauer <ga78vez@mytum.de>
2016-10-13 20:54:59 +0200

    update

--- src/sensor/filter/Kalman/gyroAngle/KalmanGyroAngleFilter.cpp
+++ src/sensor/filter/Kalman/gyroAngle/KalmanGyroAngleFilter.cpp
@@ -51,9 +51,7 @@ void KalmanGyroAngleFilter::applyFilter(double measuredRate, double dt){
     double innovation = measuredRate * dt;
     this->angle += K[0] * innovation;
     this->bias += K[1] * innovation;
-
-    this->u = K[0] * innovation / dt;
-
+    
     // (5) update P --> P(i) = (I - K * H) * P(i)
     float P00_tmp = P[0][0];
     float P01_tmp = P[0][1];
